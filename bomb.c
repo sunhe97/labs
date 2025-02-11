e5adb798f339cd1a58f44b8be3928af84770067a
HS <1246622377@qq.com>
2025-02-10 17:31:08 -0800

    add-bomb

---
+++ bomb.c
@@ -0,0 +1,399 @@
+/***************************************************************************
+ * Dr. Evil's Insidious Bomb, Version 1.1
+ * Copyright 2011, Dr. Evil Incorporated. All rights reserved.
+ *
+ * LICENSE:
+ *
+ * Dr. Evil Incorporated (the PERPETRATOR) hereby grants you (the
+ * VICTIM) explicit permission to use this bomb (the BOMB).  This is a
+ * time limited license, which expires on the death of the VICTIM.
+ * The PERPETRATOR takes no responsibility for damage, frustration,
+ * insanity, bug-eyes, carpal-tunnel syndrome, loss of sleep, or other
+ * harm to the VICTIM.  Unless the PERPETRATOR wants to take credit,
+ * that is.  The VICTIM may not distribute this bomb source code to
+ * any enemies of the PERPETRATOR.  No VICTIM may debug,
+ * reverse-engineer, run "strings" on, decompile, decrypt, or use any
+ * other technique to gain knowledge of and defuse the BOMB.  BOMB
+ * proof clothing may not be worn when handling this program.  The
+ * PERPETRATOR will not apologize for the PERPETRATOR's poor sense of
+ * humor.  This license is null and void where the BOMB is prohibited
+ * by law.
+ ***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "support.h"
+#include "phases.h"
+
+/* 
+ * Note to self: Remember to erase this file so my victims will have no
+ * idea what is going on, and so they will all blow up in a
+ * spectaculary fiendish explosion. -- Dr. Evil 
+ */
+
+FILE *infile;
+
+int main(int argc, char *argv[])
+{
+    char *input;
+
+    /* Note to self: remember to port this bomb to Windows and put a 
+     * fantastic GUI on it. */
+
+    /* When run with no arguments, the bomb reads its input lines 
+     * from standard input. */
+    if (argc == 1) {  
+	infile = stdin;
+    } 
+
+    /* When run with one argument <file>, the bomb reads from <file> 
+     * until EOF, and then switches to standard input. Thus, as you 
+     * defuse each phase, you can add its defusing string to <file> and
+     * avoid having to retype it. */
+    else if (argc == 2) {
+	if (!(infile = fopen(argv[1], "r"))) {
+	    printf("%s: Error: Couldn't open %s\n", argv[0], argv[1]);
+	    exit(8);
+	}
+    }
+
+    /* You can't call the bomb with more than 1 command line argument. */
+    else {
+	printf("Usage: %s [<input_file>]\n", argv[0]);
+	exit(8);
+    }
+
+    /* Do all sorts of secret stuff that makes the bomb harder to defuse. */
+    /*
+    * => 0x00000000004013a2 <+0>:     sub    $0x8,%rsp // return address push stack
+            0x00000000004013a6 <+4>:     mov    $0x4012a0,%esi (4199072)
+            0x00000000004013ab <+9>:     mov    $0x2,%edi (2)
+            0x00000000004013b0 <+14>:    callq  0x400b90 <signal@plt> // 是在调用signal函数，它是一个系统调用，用于设置信号处理器。@plt表示这是通过程序链接表(Procedure Linkage Table)进行的调用。
+            0x00000000004013b5 <+19>:    add    $0x8,%rsp // pop 
+            0x00000000004013b9 <+23>:    retq //return
+    */
+    initialize_bomb();
+
+    printf("Welcome to my fiendish little bomb. You have 6 phases with\n");
+    printf("which to blow yourself up. Have a nice day!\n");
+
+    /* Hmm...  Six phases must be more secure than one phase! */
+    // Border relations with Canada have never been better.
+    input = read_line();             /* Get input                   */
+    phase_1(input);                  /* Run the phase               */
+    /*
+    => 0x0000000000400ee0 <+0>:     sub    $0x8,%rsp
+   0x0000000000400ee4 <+4>:     mov    $0x402400,%esi (4203520 )
+   0x0000000000400ee9 <+9>:     callq  0x401338 <strings_not_equal>  4199224
+   0x0000000000400eee <+14>:    test   %eax,%eax
+   0x0000000000400ef0 <+16>:    je     0x400ef7 <phase_1+23>
+   0x0000000000400ef2 <+18>:    callq  0x40143a <explode_bomb>
+   0x0000000000400ef7 <+23>:    add    $0x8,%rsp
+   0x0000000000400efb <+27>:    retq  
+     */
+    phase_defused();                 /* Drat!  They figured it out!
+				      * Let me know how they did it. */
+    printf("Phase 1 defused. How about the next one?\n");
+
+    /* The second phase is harder.  No one will ever figure out
+     * how to defuse this... */
+    input = read_line();
+    /*
+    => 0x0000000000400efc <+0>:     push   %rbp
+   0x0000000000400efd <+1>:     push   %rbx
+   0x0000000000400efe <+2>:     sub    $0x28,%rsp (40 bytes)
+   0x0000000000400f02 <+6>:     mov    %rsp,%rsi (second input)
+   0x0000000000400f05 <+9>:     callq  0x40145c <read_six_numbers>
+   0x0000000000400f0a <+14>:    cmpl   $0x1,(%rsp) (rsp first value is 1)
+   0x0000000000400f0e <+18>:    je     0x400f30 <phase_2+52>
+   0x0000000000400f10 <+20>:    callq  0x40143a <explode_bomb>
+   0x0000000000400f15 <+25>:    jmp    0x400f30 <phase_2+52>
+   0x0000000000400f17 <+27>:    mov    -0x4(%rbx),%eax
+   0x0000000000400f1a <+30>:    add    %eax,%eax
+   0x0000000000400f1c <+32>:    cmp    %eax,(%rbx)
+   0x0000000000400f1e <+34>:    je     0x400f25 <phase_2+41>
+   0x0000000000400f20 <+36>:    callq  0x40143a <explode_bomb>
+   0x0000000000400f25 <+41>:    add    $0x4,%rbx
+   0x0000000000400f29 <+45>:    cmp    %rbp,%rbx
+   0x0000000000400f2c <+48>:    jne    0x400f17 <phase_2+27>
+   0x0000000000400f2e <+50>:    jmp    0x400f3c <phase_2+64>
+   0x0000000000400f30 <+52>:    lea    0x4(%rsp),%rbx rbx is address of rsp minus 4
+   0x0000000000400f35 <+57>:    lea    0x18(%rsp),%rbp rbp is address of rsp minus 24
+   0x0000000000400f3a <+62>:    jmp    0x400f17 <phase_2+27>
+   0x0000000000400f3c <+64>:    add    $0x28,%rsp
+   0x0000000000400f40 <+68>:    pop    %rbx
+   0x0000000000400f41 <+69>:    pop    %rbp
+   0x0000000000400f42 <+70>:    retq  
+     */
+     // 1 2 4 8 16 32
+    phase_2(input);
+    phase_defused();
+    printf("That's number 2.  Keep going!\n");
+
+    /* I guess this is too easy so far.  Some more complex code will
+     * confuse people. */
+    input = read_line();
+    /*
+    => 0x0000000000400f43 <+0>:     sub    $0x18,%rsp // 20 bytes
+   0x0000000000400f47 <+4>:     lea    0xc(%rsp),%rcx 
+   0x0000000000400f4c <+9>:     lea    0x8(%rsp),%rdx
+   0x0000000000400f51 <+14>:    mov    $0x4025cf,%esi "% d % d"
+   0x0000000000400f56 <+19>:    mov    $0x0,%eax
+   0x0000000000400f5b <+24>:    callq  0x400bf0 <__isoc99_sscanf@plt>
+   0x0000000000400f60 <+29>:    cmp    $0x1,%eax 
+   0x0000000000400f63 <+32>:    jg     0x400f6a <phase_3+39> jump grater > 1
+   0x0000000000400f65 <+34>:    callq  0x40143a <explode_bomb>
+   0x0000000000400f6a <+39>:    cmpl   $0x7,0x8(%rsp)
+   0x0000000000400f6f <+44>:    ja     0x400fad <phase_3+106> >7 explode so <= 7
+   0x0000000000400f71 <+46>:    mov    0x8(%rsp),%eax
+   0x0000000000400f75 <+50>:    jmpq   *0x402470(,%rax,8) // value in address 0x402470 
+   0x0000000000400f7c <+57>:    mov    $0xcf,%eax
+   0x0000000000400f81 <+62>:    jmp    0x400fbe <phase_3+123>
+   0x0000000000400f83 <+64>:    mov    $0x2c3,%eax
+   0x0000000000400f88 <+69>:    jmp    0x400fbe <phase_3+123>
+   0x0000000000400f8a <+71>:    mov    $0x100,%eax
+   0x0000000000400f8f <+76>:    jmp    0x400fbe <phase_3+123>
+   0x0000000000400f91 <+78>:    mov    $0x185,%eax
+   0x0000000000400f96 <+83>:    jmp    0x400fbe <phase_3+123>
+   0x0000000000400f98 <+85>:    mov    $0xce,%eax
+   0x0000000000400f9d <+90>:    jmp    0x400fbe <phase_3+123>
+   0x0000000000400f9f <+92>:    mov    $0x2aa,%eax
+   0x0000000000400fa4 <+97>:    jmp    0x400fbe <phase_3+123>
+   0x0000000000400fa6 <+99>:    mov    $0x147,%eax
+   0x0000000000400fab <+104>:   jmp    0x400fbe <phase_3+123>
+   0x0000000000400fad <+106>:   callq  0x40143a <explode_bomb>
+   0x0000000000400fb2 <+111>:   mov    $0x0,%eax
+   0x0000000000400fb7 <+116>:   jmp    0x400fbe <phase_3+123>
+   0x0000000000400fb9 <+118>:   mov    $0x137,%eax
+   0x0000000000400fbe <+123>:   cmp    0xc(%rsp),%eax
+   0x0000000000400fc2 <+127>:   je     0x400fc9 <phase_3+134>
+   0x0000000000400fc4 <+129>:   callq  0x40143a <explode_bomb>
+   0x0000000000400fc9 <+134>:   add    $0x18,%rsp
+   0x0000000000400fcd <+138>:   retq 
+    */
+    // 0 - 7, value  0xcf 0x2c3 0x100 0x185 0xce 0x2aa 0x147 of rcx 
+    phase_3(input);
+    phase_defused();
+    printf("Halfway there!\n");
+
+    /* Oh yeah?  Well, how good is your math?  Try on this saucy problem! */
+    /*
+    Dump of assembler code for function phase_4:
+   0x000000000040100c <+0>:     sub    $0x18,%rsp // 24 bytes
+   0x0000000000401010 <+4>:     lea    0xc(%rsp),%rcx 
+   0x0000000000401015 <+9>:     lea    0x8(%rsp),%rdx
+   0x000000000040101a <+14>:    mov    $0x4025cf,%esi
+   0x000000000040101f <+19>:    mov    $0x0,%eax
+   0x0000000000401024 <+24>:    callq  0x400bf0 <__isoc99_sscanf@plt>
+   0x0000000000401029 <+29>:    cmp    $0x2,%eax
+   0x000000000040102c <+32>:    jne    0x401035 <phase_4+41>
+   0x000000000040102e <+34>:    cmpl   $0xe,0x8(%rsp)
+   0x0000000000401033 <+39>:    jbe    0x40103a <phase_4+46>
+   0x0000000000401035 <+41>:    callq  0x40143a <explode_bomb>
+   0x000000000040103a <+46>:    mov    $0xe,%edx
+   0x000000000040103f <+51>:    mov    $0x0,%esi
+   0x0000000000401044 <+56>:    mov    0x8(%rsp),%edi
+   0x0000000000401048 <+60>:    callq  0x400fce <func4>
+   0x000000000040104d <+65>:    test   %eax,%eax
+   0x000000000040104f <+67>:    jne    0x401058 <phase_4+76>
+   0x0000000000401051 <+69>:    cmpl   $0x0,0xc(%rsp)
+   0x0000000000401056 <+74>:    je     0x40105d <phase_4+81>
+   0x0000000000401058 <+76>:    callq  0x40143a <explode_bomb>
+   0x000000000040105d <+81>:    add    $0x18,%rsp
+   0x0000000000401061 <+85>:    retq   
+End of assembler dump.
+
+// 0 -14 , binary search 
+Dump of assembler code for function func4:
+   0x0000000000400fce <+0>:     sub    $0x8,%rsp
+   0x0000000000400fd2 <+4>:     mov    %edx,%eax
+   0x0000000000400fd4 <+6>:     sub    %esi,%eax
+   0x0000000000400fd6 <+8>:     mov    %eax,%ecx
+   0x0000000000400fd8 <+10>:    shr    $0x1f,%ecx
+   0x0000000000400fdb <+13>:    add    %ecx,%eax
+   0x0000000000400fdd <+15>:    sar    %eax
+   0x0000000000400fdf <+17>:    lea    (%rax,%rsi,1),%ecx
+   0x0000000000400fe2 <+20>:    cmp    %edi,%ecx
+   0x0000000000400fe4 <+22>:    jle    0x400ff2 <func4+36>
+   0x0000000000400fe6 <+24>:    lea    -0x1(%rcx),%edx
+   0x0000000000400fe9 <+27>:    callq  0x400fce <func4>
+   0x0000000000400fee <+32>:    add    %eax,%eax
+   0x0000000000400ff0 <+34>:    jmp    0x401007 <func4+57>
+   0x0000000000400ff2 <+36>:    mov    $0x0,%eax
+   0x0000000000400ff7 <+41>:    cmp    %edi,%ecx
+   0x0000000000400ff9 <+43>:    jge    0x401007 <func4+57>
+   0x0000000000400ffb <+45>:    lea    0x1(%rcx),%esi
+   0x0000000000400ffe <+48>:    callq  0x400fce <func4>
+   0x0000000000401003 <+53>:    lea    0x1(%rax,%rax,1),%eax
+   0x0000000000401007 <+57>:    add    $0x8,%rsp
+   0x000000000040100b <+61>:    retq   
+     */
+    input = read_line();
+    phase_4(input);
+    phase_defused();
+    printf("So you got that one. Try this one.\n");
+    
+    /* Round and 'round in memory we go, where we stop, the bomb blows! */
+    input = read_line();
+    /*
+   0x0000000000401062 <+0>:     push   %rbx
+   0x0000000000401063 <+1>:     sub    $0x20,%rsp
+   0x0000000000401067 <+5>:     mov    %rdi,%rbx // rbx is input
+   0x000000000040106a <+8>:     mov    %fs:0x28,%rax
+   0x0000000000401073 <+17>:    mov    %rax,0x18(%rsp)
+   0x0000000000401078 <+22>:    xor    %eax,%eax
+   0x000000000040107a <+24>:    callq  0x40131b <string_length>
+   0x000000000040107f <+29>:    cmp    $0x6,%eax
+   0x0000000000401082 <+32>:    je     0x4010d2 <phase_5+112>
+   0x0000000000401084 <+34>:    callq  0x40143a <explode_bomb>
+   0x0000000000401089 <+39>:    jmp    0x4010d2 <phase_5+112>
+   0x000000000040108b <+41>:    movzbl (%rbx,%rax,1),%ecx // rax from 0
+   0x000000000040108f <+45>:    mov    %cl,(%rsp)
+   0x0000000000401092 <+48>:    mov    (%rsp),%rdx
+   0x0000000000401096 <+52>:    and    $0xf,%edx
+   0x0000000000401099 <+55>:    movzbl 0x4024b0(%rdx),%edx
+   0x00000000004010a0 <+62>:    mov    %dl,0x10(%rsp,%rax,1)
+   0x00000000004010a4 <+66>:    add    $0x1,%rax
+   0x00000000004010a8 <+70>:    cmp    $0x6,%rax
+   0x00000000004010ac <+74>:    jne    0x40108b <phase_5+41>
+   0x00000000004010ae <+76>:    movb   $0x0,0x16(%rsp)
+   0x00000000004010b3 <+81>:    mov    $0x40245e,%esi
+   0x00000000004010b8 <+86>:    lea    0x10(%rsp),%rdi
+   0x00000000004010bd <+91>:    callq  0x401338 <strings_not_equal>
+   0x00000000004010c2 <+96>:    test   %eax,%eax
+   0x00000000004010c4 <+98>:    je     0x4010d9 <phase_5+119>
+   0x00000000004010c6 <+100>:   callq  0x40143a <explode_bomb>
+   0x00000000004010cb <+105>:   nopl   0x0(%rax,%rax,1)
+   0x00000000004010d0 <+110>:   jmp    0x4010d9 <phase_5+119>
+   0x00000000004010d2 <+112>:   mov    $0x0,%eax
+   0x00000000004010d7 <+117>:   jmp    0x40108b <phase_5+41>
+   0x00000000004010d9 <+119>:   mov    0x18(%rsp),%rax
+   0x00000000004010de <+124>:   xor    %fs:0x28,%rax
+   0x00000000004010e7 <+133>:   je     0x4010ee <phase_5+140>
+   0x00000000004010e9 <+135>:   callq  0x400b30 <__stack_chk_fail@plt>
+   0x00000000004010ee <+140>:   add    $0x20,%rsp
+   0x00000000004010f2 <+144>:   pop    %rbx
+   0x00000000004010f3 <+145>:   retq 
+     */
+     // maduiersnfotvbyl
+     // flyers  9 15 14 5 6 7
+     // 
+    phase_5(input);
+    phase_defused();
+    printf("Good work!  On to the next...\n");
+
+    /* This phase will never be used, since no one will get past the
+     * earlier ones.  But just in case, make this one extra hard. */
+    input = read_line();
+    /*
+   0x00000000004010f4 <+0>:     push   %r14
+   0x00000000004010f6 <+2>:     push   %r13
+   0x00000000004010f8 <+4>:     push   %r12
+   0x00000000004010fa <+6>:     push   %rbp
+   0x00000000004010fb <+7>:     push   %rbx
+   0x00000000004010fc <+8>:     sub    $0x50,%rsp // 80 bytes
+   0x0000000000401100 <+12>:    mov    %rsp,%r13  // r13 point rsp
+   0x0000000000401103 <+15>:    mov    %rsp,%rsi  // rsi point rsp
+   0x0000000000401106 <+18>:    callq  0x40145c <read_six_numbers>
+   0x000000000040110b <+23>:    mov    %rsp,%r14 // r14 point rsp
+   0x000000000040110e <+26>:    mov    $0x0,%r12d // r12d to 0
+   // r13 point to data start
+
+// --------------------not same each number is small or equalthan 6//
+   0x0000000000401114 <+32>:    mov    %r13,%rbp // rbp point to data start
+   0x0000000000401117 <+35>:    mov    0x0(%r13),%eax //  eax point data start
+   0x000000000040111b <+39>:    sub    $0x1,%eax 
+   0x000000000040111e <+42>:    cmp    $0x5,%eax
+    value is below 6
+   0x0000000000401121 <+45>:    jbe    0x401128 <phase_6+52>
+   0x0000000000401123 <+47>:    callq  0x40143a <explode_bomb>
+
+   // r12d is 0, rbp value is not same with other
+   0x0000000000401128 <+52>:    add    $0x1,%r12d // value start
+   0x000000000040112c <+56>:    cmp    $0x6,%r12d
+   0x0000000000401130 <+60>:    je     0x401153 <phase_6+95>
+   0x0000000000401132 <+62>:    mov    %r12d,%ebx
+   0x0000000000401135 <+65>:    movslq %ebx,%rax
+   0x0000000000401138 <+68>:    mov    (%rsp,%rax,4),%eax
+   0x000000000040113b <+71>:    cmp    %eax,0x0(%rbp)
+   0x000000000040113e <+74>:    jne    0x401145 <phase_6+81>
+   0x0000000000401140 <+76>:    callq  0x40143a <explode_bomb>
+   0x0000000000401145 <+81>:    add    $0x1,%ebx
+   0x0000000000401148 <+84>:    cmp    $0x5,%ebx
+   0x000000000040114b <+87>:    jle    0x401135 <phase_6+65>
+
+   0x000000000040114d <+89>:    add    $0x4,%r13
+   0x0000000000401151 <+93>:    jmp    0x401114 <phase_6+32>
+// --------------------not same each number is small or equalthan 6//
+
+
+   0x0000000000401153 <+95>:    lea    0x18(%rsp),%rsi // after numbers
+   0x0000000000401158 <+100>:   mov    %r14,%rax // r14 rax is number start
+   0x000000000040115b <+103>:   mov    $0x7,%ecx
+   0x0000000000401160 <+108>:   mov    %ecx,%edx
+   0x0000000000401162 <+110>:   sub    (%rax),%edx 7 - value
+   0x0000000000401164 <+112>:   mov    %edx,(%rax)  to original value
+   0x0000000000401166 <+114>:   add    $0x4,%rax  to next pos
+   0x000000000040116a <+118>:   cmp    %rsi,%rax
+   0x000000000040116d <+121>:   jne    0x401160 <phase_6+108>
+// abse make value to 7 - x
+
+   0x000000000040116f <+123>:   mov    $0x0,%esi
+   0x0000000000401174 <+128>:   jmp    0x401197 <phase_6+163>
+   0x0000000000401176 <+130>:   mov    0x8(%rdx),%rdx
+   0x000000000040117a <+134>:   add    $0x1,%eax
+   0x000000000040117d <+137>:   cmp    %ecx,%eax
+   0x000000000040117f <+139>:   jne    0x401176 <phase_6+130>
+   0x0000000000401181 <+141>:   jmp    0x401188 <phase_6+148>
+   0x0000000000401183 <+143>:   mov    $0x6032d0,%edx
+   0x0000000000401188 <+148>:   mov    %rdx,0x20(%rsp,%rsi,2)
+   0x000000000040118d <+153>:   add    $0x4,%rsi
+   0x0000000000401191 <+157>:   cmp    $0x18,%rsi
+   0x0000000000401195 <+161>:   je     0x4011ab <phase_6+183> // JUMP OUT
+   0x0000000000401197 <+163>:   mov    (%rsp,%rsi,1),%ecx // cur value is index of list
+   0x000000000040119a <+166>:   cmp    $0x1,%ecx
+   0x000000000040119d <+169>:   jle    0x401183 <phase_6+143>
+   // INIT
+   0x000000000040119f <+171>:   mov    $0x1,%eax
+   0x00000000004011a4 <+176>:   mov    $0x6032d0,%edx
+   0x00000000004011a9 <+181>:   jmp    0x401176 <phase_6+130>
+
+
+
+   0x00000000004011ab <+183>:   mov    0x20(%rsp),%rbx
+   0x00000000004011b0 <+188>:   lea    0x28(%rsp),%rax
+   0x00000000004011b5 <+193>:   lea    0x50(%rsp),%rsi
+   0x00000000004011ba <+198>:   mov    %rbx,%rcx
+   0x00000000004011bd <+201>:   mov    (%rax),%rdx
+   0x00000000004011c0 <+204>:   mov    %rdx,0x8(%rcx)
+   0x00000000004011c4 <+208>:   add    $0x8,%rax
+   0x00000000004011c8 <+212>:   cmp    %rsi,%rax
+   0x00000000004011cb <+215>:   je     0x4011d2 <phase_6+222>
+   0x00000000004011cd <+217>:   mov    %rdx,%rcx
+   0x00000000004011d0 <+220>:   jmp    0x4011bd <phase_6+201>
+   0x00000000004011d2 <+222>:   movq   $0x0,0x8(%rdx)
+   0x00000000004011da <+230>:   mov    $0x5,%ebp
+   0x00000000004011df <+235>:   mov    0x8(%rbx),%rax
+   0x00000000004011e3 <+239>:   mov    (%rax),%eax
+   0x00000000004011e5 <+241>:   cmp    %eax,(%rbx)
+   0x00000000004011e7 <+243>:   jge    0x4011ee <phase_6+250>
+   0x00000000004011e9 <+245>:   callq  0x40143a <explode_bomb>
+   0x00000000004011ee <+250>:   mov    0x8(%rbx),%rbx
+   0x00000000004011f2 <+254>:   sub    $0x1,%ebp
+   0x00000000004011f5 <+257>:   jne    0x4011df <phase_6+235>
+   0x00000000004011f7 <+259>:   add    $0x50,%rsp
+   0x00000000004011fb <+263>:   pop    %rbx
+   0x00000000004011fc <+264>:   pop    %rbp
+   0x00000000004011fd <+265>:   pop    %r12
+   0x00000000004011ff <+267>:   pop    %r13
+   0x0000000000401201 <+269>:   pop    %r14
+   0x0000000000401203 <+271>:   retq  
+    */
+    phase_6(input);
+    phase_defused();
+
+    /* Wow, they got it!  But isn't something... missing?  Perhaps
+     * something they overlooked?  Mua ha ha ha ha! */
+    
+    return 0;
+}
